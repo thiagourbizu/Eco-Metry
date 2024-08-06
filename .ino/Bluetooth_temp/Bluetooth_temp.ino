21573da0dd4d3e39e9fe638d4a9da365268a35b0
thiagourbizu <thiago.urbizu@gmail.com>
2024-08-06 17:47:40 -0300

    .ino cambios cadena

--- .ino/Bluetooth_temp/Bluetooth_temp.ino
+++ .ino/Bluetooth_temp/Bluetooth_temp.ino
@@ -21,15 +21,17 @@ void loop() {
   // Leer el valor del sensor de temperatura
   int sensorValue = analogRead(sensorPin); // Lee el valor del sensor (0-1023)
   temperatureC = sensorValue * (5.0 / 1023.0) * 100.0; // Convierte el valor a grados Celsius
-
+  int hola = 100;
   //int roundedTempC = round(temperatureC);
 
   // También mostrar la temperatura en el monitor serial
-  Serial.println(temperatureC);
-  
+ // Serial.println(temperatureC,hola);
+  String cadena = String(temperatureC) + "," + String(hola);
   // Enviar el dato leído a través del módulo Bluetooth
-  BTSerial.print(temperatureC);
-  delay(550);
+  Serial.println(cadena);
+  
+  BTSerial.print(cadena);
+  delay(1000);
   
 }
 
